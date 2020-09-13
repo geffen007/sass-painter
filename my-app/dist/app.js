59a353dc4ac4608e96039d0e1c441e0832190182
Guido <guido.amodio@gmail.com>
2020-09-13 22:22:04 +0200

    1.2

--- my-app/dist/app.js
+++ my-app/dist/app.js
@@ -98,7 +98,6 @@ $(document).ready(function () {
     value = $(this).val();
     $('img.after').attr('style', 'clip-path: polygon(0 0%, ' + value + '% 0%, ' + value + '% 100%, 0% 100%)');
   });
-  $('.slider-container').scroll().disable();
 });
 
 /***/ }),
