38a36a63164f6d0ef96443df2aeb53538c4d8364
Guido <guido.amodio@gmail.com>
2020-09-13 21:13:31 +0200

    1.1

---
+++ my-app/src/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $(document).on('mousemove', 'input.slider', function() {
+        value=$(this).val();
+        $('img.after').attr('style', 'clip-path: polygon(0 0%, '+value+'% 0%, '+value+'% 100%, 0% 100%)');
+    });
+
+    $('.slider-container').scroll().disable();
+
+});
