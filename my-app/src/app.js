2646ce72b1f89107d39059ecc99b7899e0f83f55
Guido <guido.amodio@gmail.com>
2020-09-13 23:02:38 +0200

    1.3 goodnight

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $(document).on('mousemove', 'input.slider', function() {
         value=$(this).val();
+        value= value-0.5;
         $('img.after').attr('style', 'clip-path: polygon(0 0%, '+value+'% 0%, '+value+'% 100%, 0% 100%)');
     });
 });
