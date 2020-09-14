c5aa5c17448f18a3343a574ce9c7ff7b0195fe8a
Guido <guido.amodio@gmail.com>
2020-09-14 14:02:58 +0200

    suddivisione css

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,7 +1,9 @@
+var $ = require('jquery');
+
 $(document).ready(function() {
     $(document).on('mousemove', 'input.slider', function() {
-        value=$(this).val();
-        value= value-0.5;
+        value = $(this).val();
+        value = value - 0.5;
         $('img.after').attr('style', 'clip-path: polygon(0 0%, '+value+'% 0%, '+value+'% 100%, 0% 100%)');
     });
 });
